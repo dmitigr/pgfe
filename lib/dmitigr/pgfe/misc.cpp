40b243d4ea2f5d1394b846d5f7fcc69883a427d3
Dmitry Igrishin <dmitigr@gmail.com>
2019-02-07 12:33:00 +0300

    Use DMITIGR_ prefix

--- lib/dmitigr/pgfe/misc.cpp
+++ lib/dmitigr/pgfe/misc.cpp
@@ -9,8 +9,8 @@ namespace pgfe = dmitigr::pgfe;
 DMITIGR_PGFE_API std::int_fast32_t pgfe::version()
 {
   // Actual values are set in CMakeLists.txt.
-  constexpr std::int_least32_t major = PGFE_VERSION_PART1;
-  constexpr std::int_least32_t minor = PGFE_VERSION_PART2;
+  constexpr std::int_least32_t major = DMITIGR_PGFE_VERSION_PART1;
+  constexpr std::int_least32_t minor = DMITIGR_PGFE_VERSION_PART2;
 
   // 11.234 -> 11 * 1000 + 234 = 11234
   return major*1000 + minor;
