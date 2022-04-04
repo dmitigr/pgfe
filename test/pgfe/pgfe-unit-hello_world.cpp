7562b74618dd916b88a26cee78ef7fb2fc06a9c9
Dmitry Igrishin <dmitigr@gmail.com>
2022-04-04 17:27:09 +0300

    Licensed under the Apache License, Version 2.0
    Copyright 2022 Dmitry Igrishin
    
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
    
        http://www.apache.org/licenses/LICENSE-2.0
    
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.

--- test/pgfe/pgfe-unit-hello_world.cpp
+++ test/pgfe/pgfe-unit-hello_world.cpp
@@ -1,24 +1,18 @@
 // -*- C++ -*-
-// Copyright (C) 2022 Dmitry Igrishin
 //
-// This software is provided 'as-is', without any express or implied
-// warranty. In no event will the authors be held liable for any damages
-// arising from the use of this software.
+// Copyright 2022 Dmitry Igrishin
 //
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// 1. The origin of this software must not be misrepresented; you must not
-//    claim that you wrote the original software. If you use this software
-//    in a product, an acknowledgment in the product documentation would be
-//    appreciated but is not required.
-// 2. Altered source versions must be plainly marked as such, and must not be
-//    misrepresented as being the original software.
-// 3. This notice may not be removed or altered from any source distribution.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Dmitry Igrishin
-// dmitigr@gmail.com
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "../../src/pgfe/pgfe.hpp"
 
